966fe9fcbc7f197d43913c08916d86000364e13e
mattgulick13 <90654494+mattgulick13@users.noreply.github.com>
2021-09-21 21:39:18 -0500

    Initial commit

---
+++ forms/Form1/Form1.js
@@ -0,0 +1,25 @@
+//The code below asks the user the prompts below and then defines each variable
+let firstName1 = prompt("Enter your first name: ")
+let state1 = prompt("Enter your state of residence in its two letter abreviation: NE or FL")
+let temp1 = prompt("Enter the current temperature in degrees Farenheit: ")
+let messages1 = [
+  `Hi ${firstName1}, because the temperature is currently ${temp1} degrees you should wear a warm coat, hat, scarf and gloves.`, 
+  `Hi ${firstName1}, because the temperature is currently ${temp1} degrees you should wear a warm coat but you won't need a hat, scarf or gloves.`, 
+  `Hi ${firstName1}, because the temperature is currently ${temp1} degrees you should wear your warmest coat, a warm hat, a scarf, and warm gloves.`, 
+  `Hi ${firstName1}, because the temperature is currently ${temp1} degrees you should wear a warm coat, hat and gloves. Maybe a scarf too.`, 
+  `Please input relevant data.`
+
+  ]
+  
+  // The code below then sorts the inputs made by the user and determines which answer will pop up based on the requirements below.
+  if (state1 == 'NE' && temp1 < 32) {
+    console.log(messages1[0])
+  } else if (state1 == 'NE' && temp1 >= 32 && temp1 < 50) {
+    console.log(messages1[1])
+  } else if (state1 == 'FL' && temp1 >= 32 && temp1 < 50) {
+    console.log(messages1[2])
+  } else if (state1 == 'FL' && temp1 >= 50 && temp1 < 70)  {
+    console.log(messages1[3])
+  } else {
+    console.log(messages1[4])
+    }
